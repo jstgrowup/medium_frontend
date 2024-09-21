bb78479f5535bfa18f987f902047269498ddfb25
jstgrowup <deysubham999@gmail.com>
2024-09-22 00:21:48 +0530

    just cleaning the envs

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_PROD_BACKEND_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_SERVER_ENV === "dev"
+      ? process.env.NEXT_PUBLIC_DEV_BACKEND_URL
+      : process.env.NEXT_PUBLIC_PROD_BACKEND_URL,
   withCredentials: true,
 });
-
 export default api;
