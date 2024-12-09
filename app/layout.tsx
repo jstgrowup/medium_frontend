2fcd918d0757c2e6eca92e0fdf404fe43496e606
jstgrowup <deysubham999@gmail.com>
2024-12-09 12:00:54 +0530

    some UI fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/jpeg" href="/webpic.jpeg" />
+      </head>
       <body className={inter.className}>
         {" "}
         {children}
--- components/profile/Profile-navbar.tsx
+++ components/profile/Profile-navbar.tsx
@@ -7,6 +7,8 @@ const ProfileNavbar = () => {
   const { updateProfilePictureAction, updateProfileAction } = useAuthStore(
     (state) => state
   );
+  const { loading } = useAuthStore((state) => state);
+
   const userData = useAuthStore((state) => state.data);
   const [localImageUrl, setLocalImageUrl] = useState<string>("");
   const handleFileChange = async (
@@ -27,23 +29,33 @@ const ProfileNavbar = () => {
       console.error("File upload failed:", error);
     }
   };
+
   return (
     <div className="relative bg-gradient-to-r from-purple-600 to-blue-500 h-40">
       <div className="absolute -bottom-16 left-6">
         <div className="relative  w-[100px] h-[100px]">
-          {userData?.profilePic ? (
+          {loading ? (
+            <div className="w-full h-full flex items-center justify-center rounded-full border-4 border-white shadow-lg bg-gray-200">
+              <Loader2 className="w-8 h-8 animate-spin text-gray-500" />
+            </div>
+          ) : userData?.profilePic ? (
             <Image
-              src={userData.profilePic}
+              src={userData?.profilePic}
               alt="Profile Picture"
               className="rounded-full border-4 border-white shadow-lg"
               width={100}
               height={100}
             />
           ) : (
-            <div className="w-full h-full flex items-center justify-center rounded-full border-4 border-white shadow-lg bg-gray-200">
-              <Loader2 className="w-8 h-8 animate-spin text-gray-500" />
-            </div>
+            <Image
+              src="/no-profile.jpg"
+              alt="No Profile Picture"
+              className="rounded-full border-4 border-white shadow-lg"
+              width={100}
+              height={100}
+            />
           )}
+
           <label className="absolute -bottom-1 -right-2 bg-blue-500 rounded-full p-2 shadow-md  cursor-pointer">
             <input
               type="file"
